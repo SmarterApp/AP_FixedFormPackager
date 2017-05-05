afb09b5472111761fead43353038c80ff7c2e6de
Gregory White <gwhite@fairwaytech.com>
2017-05-05 13:55:15 -0700

    Switched application to use NUnit for testing rather than the defaulkt Microsoft suite

--- FixedFormPackager.Test/Integration/ProgramTest.cs
+++ FixedFormPackager.Test/Integration/ProgramTest.cs
@@ -6,18 +6,19 @@ using CsvHelper;
 using CsvHelper.Configuration;
 using FixedFormPackager.Test.Models;
 using FixedFormPackager.Test.Utilities;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
 
 namespace FixedFormPackager.Test.Integration
 {
-    [TestClass]
+    [TestFixture]
     public class ProgramTest
     {
         private const string OutputName = "FFP_Test";
         // TODO: Swap out this placeholder value with the relative path to the output of the FFP
         private const string InputName = "C:\\Projects\\SmarterBalanced\\Resources\\DELETE\\STS-Limited-Perf-ELA-3.xml";
+        public static readonly string ExecutionDirectory = AppDomain.CurrentDomain.BaseDirectory;
 
-        [TestInitialize]
+        [SetUp]
         public void Setup()
         {
             // Act
@@ -25,31 +26,31 @@ namespace FixedFormPackager.Test.Integration
                 $"-i \"{InputName}\" -o \"{OutputName}\" -sv");
         }
 
-        [TestMethod]
+        [Test]
         public void TestForPresenceOfSevereErrors()
         {
             // Act
-            var result = GetErrorList($"{Directory.GetCurrentDirectory()}\\{OutputName}.errors.csv");
+            var result = GetErrorList($"{ExecutionDirectory}\\{OutputName}.errors.csv");
 
             // Assert
             Assert.IsTrue(!result.Any(x => x.ErrorSeverity.Equals("Severe", StringComparison.OrdinalIgnoreCase)));
         }
 
-        [TestMethod]
+        [Test]
         public void TestForPresenceOfDegradedErrors()
         {
             // Act
-            var result = GetErrorList($"{Directory.GetCurrentDirectory()}\\{OutputName}.errors.csv");
+            var result = GetErrorList($"{ExecutionDirectory}\\{OutputName}.errors.csv");
 
             // Assert
             Assert.IsTrue(!result.Any(x => x.ErrorSeverity.Equals("Degraded", StringComparison.OrdinalIgnoreCase)));
         }
 
-        [TestMethod]
+        [Test]
         public void TestForPresenceOfBenignErrors()
         {
             // Act
-            var result = GetErrorList($"{Directory.GetCurrentDirectory()}\\{OutputName}.errors.csv");
+            var result = GetErrorList($"{ExecutionDirectory}\\{OutputName}.errors.csv");
 
             // Assert
             Assert.IsTrue(!result.Any(x => x.ErrorSeverity.Equals("Benign", StringComparison.OrdinalIgnoreCase)));
