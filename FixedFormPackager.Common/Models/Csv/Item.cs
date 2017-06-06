b99d5a29090b9ae55fef32973bcfa45ed4f13f1c
Gregory White <gwhite@fairwaytech.com>
2017-06-05 18:50:07 -0700

    Remove stim property from item model

--- FixedFormPackager.Common/Models/Csv/Item.cs
+++ FixedFormPackager.Common/Models/Csv/Item.cs
@@ -5,7 +5,6 @@ namespace FixedFormPackager.Common.Models.Csv
     public class Item
     {
         public string ItemId { get; set; }
-        public string AssociatedStimuliId { get; set; }
         public string FormPartitionId { get; set; }
         public string FormPartitionPosition { get; set; }
         public string FormPosition { get; set; }
