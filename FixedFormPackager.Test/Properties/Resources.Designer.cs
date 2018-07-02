28b7de334f3e8ffdd63425c593a1e432821e4c38
Steve Mock <smock@fairwaytech.com>
2018-07-02 14:25:03 -0500

    Making the solution actually build.

--- FixedFormPackager.Test/Properties/Resources.Designer.cs
+++ FixedFormPackager.Test/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace FixedFormPackager.Test.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -91,7 +91,8 @@ namespace FixedFormPackager.Test.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to &lt;?xml version=&quot;1.0&quot; encoding=&quot;utf-8&quot; ?&gt;
+        ///   Looks up a localized string similar to &lt;?xml version=&quot;1.0&quot; encoding=&quot;utf-8&quot;?&gt;
+        ///
         ///&lt;nlog xmlns=&quot;http://www.nlog-project.org/schemas/NLog.xsd&quot;
         ///      xmlns:xsi=&quot;http://www.w3.org/2001/XMLSchema-instance&quot;
         ///      xsi:schemaLocation=&quot;http://www.nlog-project.org/schemas/NLog.xsd NLog.xsd&quot;
@@ -102,7 +103,7 @@ namespace FixedFormPackager.Test.Properties {
         ///  &lt;!-- optional, add some variables
         ///  https://github.com/nlog/NLog/wiki/Configuration-file#variables
         ///  --&gt;
-        ///  &lt;variable name=&quot;myvar&quot;  [rest of string was truncated]&quot;;.
+        ///  &lt;variable name=&quot;myvar&quot; [rest of string was truncated]&quot;;.
         /// </summary>
         internal static string NLog {
             get {
@@ -122,19 +123,23 @@ namespace FixedFormPackager.Test.Properties {
         
         /// <summary>
         ///   Looks up a localized string similar to &lt;?xml version=&quot;1.0&quot;?&gt;
+        ///
         ///&lt;doc&gt;
-        ///    &lt;assembly&gt;
-        ///        &lt;name&gt;NLog&lt;/name&gt;
-        ///    &lt;/assembly&gt;
-        ///    &lt;members&gt;
-        ///        &lt;member name=&quot;T:JetBrains.Annotations.CanBeNullAttribute&quot;&gt;
-        ///            &lt;summary&gt;
-        ///            Indicates that the value of the marked element could be &lt;c&gt;null&lt;/c&gt; sometimes,
-        ///            so the check for &lt;c&gt;null&lt;/c&gt; is necessary before its usage
-        ///            &lt;/summary&gt;
-        ///            &lt;example&gt;&lt;code&gt;
-        ///            [CanBeNull] public object Test() { return null; }
-        ///            public void UseTest() {        /// [rest of string was truncated]&quot;;.
+        ///  &lt;assembly&gt;
+        ///    &lt;name&gt;NLog&lt;/name&gt;
+        ///  &lt;/assembly&gt;
+        ///  &lt;members&gt;
+        ///    &lt;member name=&quot;T:JetBrains.Annotations.CanBeNullAttribute&quot;&gt;
+        ///      &lt;summary&gt;
+        ///        Indicates that the value of the marked element could be &lt;c&gt;null&lt;/c&gt; sometimes,
+        ///        so the check for &lt;c&gt;null&lt;/c&gt; is necessary before its usage
+        ///      &lt;/summary&gt;
+        ///      &lt;example&gt;
+        ///        &lt;code&gt;
+        ///          [CanBeNull] public object Test() { return null; }
+        ///          public void UseTest() {
+        ///          var p = Test();
+        ///     [rest of string was truncated]&quot;;.
         /// </summary>
         internal static string NLog2 {
             get {
@@ -143,93 +148,13 @@ namespace FixedFormPackager.Test.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized resource of type System.Byte[].
-        /// </summary>
-        internal static byte[] ProcessSmarterTestPackage {
-            get {
-                object obj = ResourceManager.GetObject("ProcessSmarterTestPackage", resourceCulture);
-                return ((byte[])(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Byte[].
-        /// </summary>
-        internal static byte[] ProcessSmarterTestPackage1 {
-            get {
-                object obj = ResourceManager.GetObject("ProcessSmarterTestPackage1", resourceCulture);
-                return ((byte[])(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Byte[].
-        /// </summary>
-        internal static byte[] SmarterTestPackage_Common {
-            get {
-                object obj = ResourceManager.GetObject("SmarterTestPackage_Common", resourceCulture);
-                return ((byte[])(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Byte[].
-        /// </summary>
-        internal static byte[] SmarterTestPackage_Common1 {
-            get {
-                object obj = ResourceManager.GetObject("SmarterTestPackage_Common1", resourceCulture);
-                return ((byte[])(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Byte[].
-        /// </summary>
-        internal static byte[] TabulateSmarterTestAdminPackage {
-            get {
-                object obj = ResourceManager.GetObject("TabulateSmarterTestAdminPackage", resourceCulture);
-                return ((byte[])(obj));
-            }
-        }
-        
-        /// <summary>
         ///   Looks up a localized string similar to &lt;?xml version=&quot;1.0&quot; encoding=&quot;utf-8&quot;?&gt;
+        ///
         ///&lt;configuration&gt;
-        ///    &lt;startup&gt; 
-        ///        &lt;supportedRuntime version=&quot;v4.0&quot; sku=&quot;.NETFramework,Version=v4.6.1&quot;/&gt;
-        ///    &lt;/startup&gt;
-        ///&lt;/configuration&gt;
-        ///.
-        /// </summary>
-        internal static string TabulateSmarterTestAdminPackage_dll {
-            get {
-                return ResourceManager.GetString("TabulateSmarterTestAdminPackage_dll", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to &lt;?xml version=&quot;1.0&quot; encoding=&quot;utf-8&quot;?&gt;
-        ///&lt;configuration&gt;
-        ///    &lt;startup&gt; 
-        ///        &lt;supportedRuntime version=&quot;v4.0&quot; sku=&quot;.NETFramework,Version=v4.6.1&quot;/&gt;
-        ///    &lt;/startup&gt;
-        ///&lt;/configuration&gt;
-        ///.
-        /// </summary>
-        internal static string TabulateSmarterTestAdminPackage_exe {
-            get {
-                return ResourceManager.GetString("TabulateSmarterTestAdminPackage_exe", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to &lt;?xml version=&quot;1.0&quot; encoding=&quot;utf-8&quot;?&gt;
-        ///&lt;configuration&gt;
-        ///    &lt;startup&gt; 
-        ///        &lt;supportedRuntime version=&quot;v4.0&quot; sku=&quot;.NETFramework,Version=v4.6.1&quot;/&gt;
-        ///    &lt;/startup&gt;
-        ///&lt;/configuration&gt;
-        ///.
+        ///  &lt;startup&gt;
+        ///    &lt;supportedRuntime version=&quot;v4.0&quot; sku=&quot;.NETFramework,Version=v4.6.1&quot; /&gt;
+        ///  &lt;/startup&gt;
+        ///&lt;/configuration&gt;.
         /// </summary>
         internal static string TabulateSmarterTestAdminPackage_vshost_exe {
             get {
@@ -243,16 +168,6 @@ namespace FixedFormPackager.Test.Properties {
         internal static byte[] TabulateSmarterTestAdminPackage_vshost_exe1 {
             get {
                 object obj = ResourceManager.GetObject("TabulateSmarterTestAdminPackage_vshost_exe1", resourceCulture);
-                return ((byte[])(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Byte[].
-        /// </summary>
-        internal static byte[] TabulateSmarterTestAdminPackage1 {
-            get {
-                object obj = ResourceManager.GetObject("TabulateSmarterTestAdminPackage1", resourceCulture);
                 return ((byte[])(obj));
             }
         }
@@ -318,41 +233,11 @@ namespace FixedFormPackager.Test.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized resource of type System.Byte[].
-        /// </summary>
-        internal static byte[] TabulateSmarterTestPackage1 {
-            get {
-                object obj = ResourceManager.GetObject("TabulateSmarterTestPackage1", resourceCulture);
-                return ((byte[])(obj));
-            }
-        }
-        
-        /// <summary>
         ///   Looks up a localized string similar to test.
         /// </summary>
         internal static string test {
             get {
                 return ResourceManager.GetString("test", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Byte[].
-        /// </summary>
-        internal static byte[] ValidateSmarterTestPackage {
-            get {
-                object obj = ResourceManager.GetObject("ValidateSmarterTestPackage", resourceCulture);
-                return ((byte[])(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Byte[].
-        /// </summary>
-        internal static byte[] ValidateSmarterTestPackage1 {
-            get {
-                object obj = ResourceManager.GetObject("ValidateSmarterTestPackage1", resourceCulture);
-                return ((byte[])(obj));
             }
         }
     }
